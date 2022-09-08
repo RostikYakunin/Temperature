163bf1e7f8022191d0cbf02f81ecd402c44e8058
Rostyslav Yakunin <rostishka1310942@gmail.com>
2022-09-08 16:38:48 +0300

    update: replacing russian interface with English

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("1.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 364, 400);
-        stage.setTitle("Temperature");
+        stage.setTitle("Weather Viewer");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
